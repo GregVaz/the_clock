03c9dea624717d014b95defb00d085cb1f7753ad
Gregorio <gregoriovazya@gmail.com>
2021-04-14 07:51:06 -0600

    Change IP for production environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :rails_env, :production
 set :branch, "master"
-server '3.133.58.216', user: 'deploy', roles: %w{web app db}
+server '3.16.89.42', user: 'deploy', roles: %w{web app db}
